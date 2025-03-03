731d5b614696492994625e721f0ce1f1eadee7b9
malobs <malobamercy958@gmail.com>
2025-03-03 22:16:05 +0300

    Building a Modal Window Using a React Portal

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,26 +1,48 @@
 import Button from "../../ui/Button";
-import { useState } from "react";
+// import { useState } from "react";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
+import CabinTable from "./CabinTable";
 
-const AddCabin = () => {
-  const [isOpenModal, setIsOpenModal] = useState(false);
+function AddCabin() {
   return (
-    <div>
-      {" "}
-      <Button
-        onClick={() => setIsOpenModal((show) => !show)}
-        style={{ marginTop: "2%", width: "100%", marginBottom: "2%" }}
-      >
-        Add new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+      <Modal.Open opens="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   );
-};
+}
 
 export default AddCabin;
+
+// const AddCabin = () => {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+//   return (
+//     <div>
+//       {" "}
+//       <Button
+//         onClick={() => setIsOpenModal((show) => !show)}
+//         style={{ marginTop: "2%", width: "100%", marginBottom: "2%" }}
+//       >
+//         Add new cabin
+//       </Button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default AddCabin;
