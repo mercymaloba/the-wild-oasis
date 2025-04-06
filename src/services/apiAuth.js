203f71877a9787c138c99c846e9a3f835bf402db
malobs <malobamercy958@gmail.com>
2025-04-07 00:21:29 +0300

    user sign up

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,21 +1,21 @@
 import supabase from "./supabase";
 
-// export async function signup({fullName, email, password }) {
-//   const { data, error } = await supabase.auth.signUp({
-//     email,
-//     password,
-//     options: {
-//       data: {
-//         fullName,
-//         avatar: "",
-//       },
-//     },
-//   });
+export async function signup({ fullName, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
 
-//   if (error) throw new Error(error.message);
+  if (error) throw new Error(error.message);
 
-//   return data;
-// }
+  return data;
+}
 
 export async function login({ email, password }) {
   const { data, error } = await supabase.auth.signInWithPassword({
