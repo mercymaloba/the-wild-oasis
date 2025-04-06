3b2dfe028080a2524804c62b41b28e80eadced97
malobs <malobamercy958@gmail.com>
2025-04-06 23:39:46 +0300

    authorization:protecting route

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -12,7 +12,15 @@ function LoginForm() {
   function handleSubmit(e) {
     e.preventDefault();
     if (!email || !password) return;
-    login({ email, password });
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setEmail("");
+          setPassword("");
+        },
+      }
+    );
   }
 
   return (
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -28,15 +28,15 @@ export async function login({ email, password }) {
   return data;
 }
 
-// export async function getCurrentUser() {
-//   const { data: session } = await supabase.auth.getSession();
-//   if (!session.session) return null;
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
 
-//   const { data, error } = await supabase.auth.getUser();
+  const { data, error } = await supabase.auth.getUser();
 
-//   if (error) throw new Error(error.message);
-//   return data?.user;
-// }
+  if (error) throw new Error(error.message);
+  return data?.user;
+}
 
 // export async function logout() {
 //   const { error } = await supabase.auth.signOut();
