35ae9520ec5b02ae65041814c79099514b9c26a5
malobs <malobamercy958@gmail.com>
2025-04-11 21:41:04 +0300

    displaying statistics

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 import { useRecentStays } from "./useRecentStays";
 import { useRecentBookings } from "./useRecentBookings";
-// import Spinner from "../../ui/Spinner";
-// import Stats from "./Stats";
-// import { useCabins } from "../cabins/useCabins";
+import Spinner from "../../ui/Spinner";
+import Stats from "./Stats";
+import { useCabins } from "../cabins/useCabins";
 // import SalesChart from "./SalesChart";
 // import DurationChart from "./DurationChart";
 // import TodayActivity from "../check-in-out/TodayActivity";
@@ -18,18 +18,18 @@ const StyledDashboardLayout = styled.div`
 function DashboardLayout() {
   const { bookings, isLoading: isLoading1 } = useRecentBookings();
   const { confirmedStays, isLoading: isLoading2, numDays } = useRecentStays();
-  // const { cabins, isLoading: isLoading3 } = useCabins();
+  const { cabins, isLoading: isLoading3 } = useCabins();
 
-  // if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
 
   return (
     <StyledDashboardLayout>
-      {/* <Stats
+      <Stats
         bookings={bookings}
         confirmedStays={confirmedStays}
         numDays={numDays}
         cabinCount={cabins.length}
-      /> */}
+      />
       {/* <TodayActivity /> */}
       {/* <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} /> */}
